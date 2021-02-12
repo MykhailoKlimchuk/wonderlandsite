2b9924c8eb14ca7b8ef54907f420ca421fbac2b6
38067 <mikeklimchuck@gmail.com>
2021-02-12 11:11:18 +0200

    update

--- WonderlandSite/Games/views.py
+++ WonderlandSite/Games/views.py
@@ -11,19 +11,7 @@ from .forms import ContactUsForm
 class GameView(View):
     def get(self, request):
         #     todo зробити через штмл адаптивну таблицю
-        games = Game.objects.all()
-        games_list = []
-        i = 0
-        j = 3
-        while True:
-            games_list.append(games[i:j])
-            i += 3
-            j += 3
-            if j == len(games):
-                break
-            if j > len(games):
-                games_list.append(games[i:len(games)])
-            break
+        games_list = Game.objects.all()
 
         return render(request, "games/games_list.html", {"games_list": games_list})
 
@@ -51,19 +39,7 @@ class HomeView(View):
 class TeamView(View):
     def get(self, request):
         #     todo зробити через штмл адаптивну таблицю
-        teammates = Teammate.objects.all()
-        teammates_list = []
-        i = 0
-        j = 3
-        while True:
-            teammates_list.append(teammates[i:j])
-            i += 3
-            j += 3
-            if j == len(teammates):
-                break
-            if j > len(teammates):
-                teammates_list.append(teammates[i:len(teammates)])
-            break
+        teammates_list = Teammate.objects.all()
         return render(request, "team.html", {"teammates_list": teammates_list})
 
 
